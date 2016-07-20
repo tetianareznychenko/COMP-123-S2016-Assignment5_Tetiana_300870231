307bed224987a719523b30ff53daed3f07456ff1
tetianareznychenko <tatiana.reznuchenko@gmail.com>
2016-07-20 11:15:39 -0400

    switch

--- Assignment05/Program.cs
+++ Assignment05/Program.cs
@@ -11,44 +11,96 @@ namespace Assignment05
     {
         static void Main(string[] args)
         {
-
-            DisplayMenu();
-            int userInput = 0;
-            do
+            try
             {
-                if (userInput == 1)
+                int menuchoice = 0;
+                while (menuchoice != 2)
                 {
 
+                    Console.WriteLine("Choose your option");
+                    Console.WriteLine();
+                    Console.WriteLine("1. Display Grades");
+                    Console.WriteLine("2. Exit");
+                    Console.WriteLine();
+
+                    menuchoice = int.Parse(Console.ReadLine());
+
+                    switch (menuchoice)
+                    {
+                        case 1:
+                            const string FILENAME = "studentInfo.txt";
+                            const char DELIM = ',';
+                            string lastName;
+                            string firstName;
+                            string numberID;
+                            string course;
+                            string grade;
+
+                            Console.WriteLine("Enter the file name: ");
+                            string userFile = Convert.ToString(Console.ReadLine());
+                            if (userFile == FILENAME)
+                            {
+                                // opening filestream
+                                FileStream inFile = new FileStream(FILENAME, FileMode.Open, FileAccess.Read);
+                                StreamReader reader = new StreamReader(inFile); 
+
+                                
+
+                                    // setup variables to temporariliy hold my data
+                                    string recordString;
+                                string[] fields;
+
+                                // read from file and assign the record to recordIN
+                                recordString = reader.ReadLine();
+
+                                while (recordString != null)
+                                {
+
+                                    fields = recordString.Split(DELIM);
+                                    lastName = fields[0];
+                                    firstName = fields[1];
+                                    numberID = fields[2];
+                                    course = fields[3];
+                                    grade = fields[4];
+
+                                    Console.WriteLine("Last Name", "Firs tName", "Firs tName", "ID#", "Class", "Grade");
+                                    Console.WriteLine("{0} {1} {2} {3} {4}",
+                                        lastName,
+                                        firstName,
+                                        numberID,
+                                        course,
+                                        grade);
+
+                                    recordString = reader.ReadLine();
+
+                                    // close streams
+                                    reader.Close();
+                                    inFile.Close();
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("File does not exists");
+                            }
+                            break;
+                        case 2:
+                            break;
+                        default:
+                            Console.WriteLine("Sorry, invalid selection");
+                            break;
+                    }
                 }
+
             }
-            while (userInput != 2);
-
-            string lastName;
-            string firstName;
-            int numberID;
-            string course;
-            string grade;
-
-            string myFile = "studentInfo.txt";
-
-            if (File.Exists(myFile))
+            catch (Exception exception)
             {
-                Console.WriteLine(File.GetCreationTime(myFile));
+
+                Console.WriteLine("Error: " + exception.Message);
             }
-
-
-
         }
 
-        static public int DisplayMenu()
-        {
-            Console.WriteLine("Choose your option");
-            Console.WriteLine();
-            Console.WriteLine("1. Display Grades");
-            Console.WriteLine("2. Exit");
-            Console.WriteLine();
-            var result = Console.ReadLine();
-            return Convert.ToInt32(result);
-        }
+
+    
+                     
     }
 }
