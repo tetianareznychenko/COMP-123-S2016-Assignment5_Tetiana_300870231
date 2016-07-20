c6291f381a89f09e423f7f341e5f049956b78c26
tetianareznychenko <tatiana.reznuchenko@gmail.com>
2016-07-20 10:15:27 -0400

    DisplayMenu() method

--- Assignment05/Program.cs
+++ Assignment05/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Assignment05
 {
@@ -10,6 +11,44 @@ namespace Assignment05
     {
         static void Main(string[] args)
         {
+
+            DisplayMenu();
+            int userInput = 0;
+            do
+            {
+                if (userInput == 1)
+                {
+
+                }
+            }
+            while (userInput != 2);
+
+            string lastName;
+            string firstName;
+            int numberID;
+            string course;
+            string grade;
+
+            string myFile = "studentInfo.txt";
+
+            if (File.Exists(myFile))
+            {
+                Console.WriteLine(File.GetCreationTime(myFile));
+            }
+
+
+
+        }
+
+        static public int DisplayMenu()
+        {
+            Console.WriteLine("Choose your option");
+            Console.WriteLine();
+            Console.WriteLine("1. Display Grades");
+            Console.WriteLine("2. Exit");
+            Console.WriteLine();
+            var result = Console.ReadLine();
+            return Convert.ToInt32(result);
         }
     }
 }
